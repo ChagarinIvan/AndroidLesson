41c0aa72f9e3c597ee319994bbbf72064ecc863d
ChagarinIvan <Chagarin.Ivan@gmail.com>
2017-01-13 15:06:37 +0200

    add color nad data

--- app/src/main/java/by/chagarin/androidlesson/TransactionAdapter.java
+++ app/src/main/java/by/chagarin/androidlesson/TransactionAdapter.java
@@ -1,18 +1,17 @@
 package by.chagarin.androidlesson;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
-/**
- * Created by IME on 07.01.2017.
- */
 
 public class TransactionAdapter extends ArrayAdapter<Transaction> {
     private List<Transaction> transactions;
@@ -29,10 +28,19 @@ public class TransactionAdapter extends ArrayAdapter<Transaction> {
         if (convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
+        RelativeLayout listItem = (RelativeLayout) convertView.findViewById(R.id.listItem);
         TextView title = (TextView) convertView.findViewById(R.id.title);
         TextView price = (TextView) convertView.findViewById(R.id.price);
+        TextView date = (TextView) convertView.findViewById(R.id.date);
+        if (position % 2 == 0){
+            listItem.setBackgroundColor(Color.RED);
+        } else {
+            listItem.setBackgroundColor(Color.GREEN);
+        }
         title.setText(transaction.getTitle());
         price.setText(transaction.getPrice());
+        date.setText(transaction.getDate());
+
         return convertView;
     }
 }
