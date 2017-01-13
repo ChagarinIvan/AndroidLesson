d631f0d01a96457f78ea67826d53f53597cd80b1
ChagarinIvan <Chagarin.Ivan@gmail.com>
2017-01-14 00:25:40 +0200

    add fragment

--- app/src/main/java/by/chagarin/androidlesson/MainActivity.java
+++ app/src/main/java/by/chagarin/androidlesson/MainActivity.java
@@ -10,33 +10,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private TransactionAdapter transactionAdapter;
-    private List<Transaction> data = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<Transaction> adapterData = getData();
-        transactionAdapter = new TransactionAdapter(this,adapterData);
-        listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(transactionAdapter);
-    }
 
-    private List<Transaction> getData(){
-        try {
-            data.add(new Transaction("Windows",50, "2001-07-25"));
-            data.add(new Transaction("Phone",20, "2010-10-23"));
-            data.add(new Transaction("iOS",100, "2019-03-24"));
-            data.add(new Transaction("Mac",120, "2000-02-26"));
-            data.add(new Transaction("Linux",150, "1989-01-27"));
-            data.add(new Transaction("Linux",150, "1989-01-27"));
-            data.add(new Transaction("Linux",150, "1989-01-27"));
-            data.add(new Transaction("Linux",150, "1989-01-27"));
-        } catch (ParseException e) {
-
-        }
-        return data;
     }
 }
