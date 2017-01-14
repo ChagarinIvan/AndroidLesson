b697fda3525c3d294b0fc17f9f8aaa5103292cbb
ChagarinIvan <Chagarin.Ivan@gmail.com>
2017-01-14 22:45:21 +0200

    add novigate panel and toolbar

---
+++ app/src/main/java/by/chagarin/androidlesson/Category.java
@@ -0,0 +1,17 @@
+package by.chagarin.androidlesson;
+
+/**
+ * Created by IME on 14.01.2017.
+ */
+
+public class Category {
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- app/src/main/java/by/chagarin/androidlesson/MainActivity.java
+++ app/src/main/java/by/chagarin/androidlesson/MainActivity.java
@@ -1,19 +1,85 @@
 package by.chagarin.androidlesson;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ListView left_drawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        left_drawer = (ListView) findViewById(R.id.left_drawer);
 
+//        String[] navigationData = getResources().getStringArray(R.array.screen_aray);
+        String[] navigationData = new String[]{
+                getString(R.string.transactions),
+                getString(R.string.categores),
+                getString(R.string.statistics)};
+        ArrayAdapter<String> navigationDrawerAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, navigationData);
+        left_drawer.setAdapter(navigationDrawerAdapter);
+        left_drawer.setOnItemClickListener(new DrawerItemClickListener());
+
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.setDrawerListener(drawerToggle);
+
+
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            switch (position) {
+                case 0:
+                    left_drawer.setItemChecked(position, true);
+                    drawerLayout.closeDrawer(left_drawer);
+                    setTitle(getString(R.string.transactions));
+                    getFragmentManager().beginTransaction().replace(R.id.content_frame, new TransactionsFragment()).commit();
+                    break;
+                case 1:
+                    left_drawer.setItemChecked(position, true);
+                    drawerLayout.closeDrawer(left_drawer);
+                    setTitle(getString(R.string.categores));
+                    getFragmentManager().beginTransaction().replace(R.id.content_frame, new CategoresFragment()).commit();
+                    break;
+                case 2:
+                    left_drawer.setItemChecked(position, true);
+                    drawerLayout.closeDrawer(left_drawer);
+                    setTitle(getString(R.string.statistics));
+                    getFragmentManager().beginTransaction().replace(R.id.content_frame, new StatisticsFragment()).commit();
+                    break;
+            }
+        }
     }
 }
