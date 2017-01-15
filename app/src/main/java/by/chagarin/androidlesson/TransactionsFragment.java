7e24117a7e91e5d733e150d91e11b3f43823fba4
ChagarinIvan <Chagarin.Ivan@gmail.com>
2017-01-16 01:06:40 +0200

    add add transaction activity

--- app/src/main/java/by/chagarin/androidlesson/TransactionsFragment.java
+++ app/src/main/java/by/chagarin/androidlesson/TransactionsFragment.java
@@ -1,6 +1,7 @@
 package by.chagarin.androidlesson;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,7 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.melnykov.fab.FloatingActionButton;
 
@@ -43,8 +43,8 @@ public class TransactionsFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), AddTransactionActivity.class);
+                getActivity().startActivity(intent);
             }
         });
         return inflate;
