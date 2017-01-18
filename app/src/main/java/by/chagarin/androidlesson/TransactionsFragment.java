0eae0d9fee18309e51a90867a1b6a2f436fe968a
ChagarinIvan <Chagarin.Ivan@gmail.com>
2017-01-18 10:24:51 +0200

    add add transaction activity

--- app/src/main/java/by/chagarin/androidlesson/TransactionsFragment.java
+++ app/src/main/java/by/chagarin/androidlesson/TransactionsFragment.java
@@ -43,7 +43,7 @@ public class TransactionsFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AddTransactionActivity.class);
+                Intent intent = new Intent(getActivity(), AddTransactionActivity_.class);
                 getActivity().startActivity(intent);
             }
         });
