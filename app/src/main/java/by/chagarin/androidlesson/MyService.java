5d0ffc7df4ce7d45e1e2387a55378e49f2e734bc
ChagarinIvan <Chagarin.Ivan@gmail.com>
2017-02-03 00:10:44 +0200

    add pieChat to static_fragment

--- app/src/main/java/by/chagarin/androidlesson/MyService.java
+++ app/src/main/java/by/chagarin/androidlesson/MyService.java
@@ -4,20 +4,23 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 
 import org.androidannotations.annotations.EService;
-import org.androidannotations.annotations.SystemService;
 import org.androidannotations.annotations.UiThread;
 
 import java.util.concurrent.TimeUnit;
 
 @EService
 public class MyService extends Service {
-    @SystemService
-    NotificationManager notificationManager;
+    private static final int NOTIFY_ID = 101;
 
     public MyService() {
         super();
@@ -49,25 +52,50 @@ public class MyService extends Service {
     void someTask() {
         new Thread(new Runnable() {
             public void run() {
-                for (int i = 1; i <= 150; i++) {
+                //noinspection InfiniteLoopStatement
+                while (true) {
                     try {
-                        TimeUnit.SECONDS.sleep(3);
+                        TimeUnit.HOURS.sleep(8);
                         showToast();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                stopSelf();
             }
         }).start();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @UiThread
     void showToast() {
-        Notification notif = new Notification(R.drawable.ic_add_white_24dp, "Text in status bar", System.currentTimeMillis());
+        Context context = getApplicationContext();
 
-        Intent intent = new Intent(this, MainActivity_.class);
-        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Intent notificationIntent = new Intent(context, MainActivity_.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
 
+        Resources res = context.getResources();
+        Notification.Builder builder = new Notification.Builder(context);
+
+        builder.setContentIntent(contentIntent)
+                .setSmallIcon(R.drawable.ic_attach_money)
+                // большая картинка
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.drawer_header))
+                //.setTicker(res.getString(R.string.warning)) // текст в строке состояния
+                .setTicker(getString(R.string.alarm_message))
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                //.setContentTitle(res.getString(R.string.notifytitle)) // Заголовок уведомления
+                .setContentTitle(getString(R.string.notification_title))
+                //.setContentText(res.getString(R.string.notifytext))
+                .setContentText(getString(R.string.notifaction_text)); // Текст уведомления
+
+        // Notification notification = builder.getNotification(); // до API 16
+        Notification notification = builder.build();
+
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFY_ID, notification);
     }
 }
